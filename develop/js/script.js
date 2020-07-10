3cdaa6ae4af310eea4a76dcbd8ed9212c85327cc
Maya Chisem <chisem.maya@gmail.com>
2020-07-10 15:24:32 -0700

    update

--- develop/js/script.js
+++ develop/js/script.js
@@ -14,3 +14,39 @@ if (areYouReady) {
     alert("Well then.");
   }
 
+// variables
+var length = document.querySelector("#length");
+var lowerCase = document.querySelector("#lowercase");
+var upperCase = document.querySelector("#uppercase");
+var numbers = document.querySelector("#numbers");
+var specialCharacters = document.querySelector("#special-characters");
+
+const characters = '~`!@#$%^&*(){}[]=<>/,.""'
+const upperLetters = [
+    'A','B','C','D','E','F','G','H','I','J','K','L','M','N',
+    'O','P','Q','R','S','T','U','V','W','X','Y','Z'
+]
+const lowerLetters = [
+    'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o',
+    'p','q','r','s','t','u','v','w','x','y','z'
+]
+
+
+function generatePassword() {
+    
+    const finalPassword = generatedPassword.slice(0, length);
+	
+	return finalPassword;
+
+}
+
+// when you click the generate button the password prompts to the user
+generate.addEventListener('click', () => {
+	const passLength = +length.value;
+	const hasLower = lowerCase.checked;
+	const hasUpper = upperCase.checked;
+	const hasNumber = numbers.checked;
+	const hasSymbol = specialCharacters.checked;
+    
+    alert(generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, passLength));
+});
