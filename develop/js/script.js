3d0d1a5c92970688ea23031ef0411a1c113b29c3
Maya Chisem <chisem.maya@gmail.com>
2020-07-10 15:01:00 -0700

    updating

---
+++ develop/js/script.js
@@ -0,0 +1,16 @@
+// greeting
+alert("Welcome to the Password Generator!");
+
+// confirm if user wants to make a password
+var areYouReady = confirm("Ready to begin?");
+
+// conditional responses based on whether the user wants to make a password or not
+if (areYouReady) {
+    var instructions = confirm("Check the boxes below to customize your new password.");
+;
+  }
+
+  else {
+    alert("Well then.");
+  }
+
