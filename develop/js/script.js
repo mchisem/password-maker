85690b06cce6522b7c7a6b0a6442173f5519927a
Maya Chisem <chisem.maya@gmail.com>
2020-07-22 17:09:17 -0700

    update

--- develop/js/script.js
+++ develop/js/script.js
@@ -4,7 +4,7 @@ alert("Welcome to the Password Generator!");
 // confirm if user wants to make a password
 var areYouReady = confirm("Ready to begin?");
 
-// conditional responses based on whether the user wants to make a password or not
+// // conditional responses based on whether the user wants to make a password or not
 if (areYouReady) {
     var instructions = confirm("Check the boxes below to customize your new password.");
   } else {
@@ -12,77 +12,74 @@ if (areYouReady) {
   }
 
 // variables
-var length = document.querySelector("#length");
-var lowerCase = document.querySelector("#lowercase");
+var passLength = document.querySelector("#length");
 var upperCase = document.querySelector("#uppercase");
+var lowerCase = document.querySelector("#lowercase");
 var numbers = document.querySelector("#numbers");
-var specialCharacters = document.querySelector("#special-characters");
-var generate = document.querySelector("#generate-btn");
+var symbols = document.querySelector("#symbols");
+var generateBtn = document.querySelector("#generate");
+var result = document.querySelector("#result");
 
-const upperLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-
-const lowerLetters = 'abcdefghijklmnopqrstuvwxyz'
-
-const characters = '~`!@#$%^&*(){}[]=<>/,.""'
-
-// functions to get random elements
-function getRandomLower() {
-	return String.fromCharCode(Math.floor(Math.random() * 26) + lowerLetters.length);
-}
-
-function getRandomUpper() {
-	return String.fromCharCode(Math.floor(Math.random() * 26) + upperLetters.length);
-}
-
-function getRandomNumber() {
-	return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-
-function getRandomCharacter() {
-	return characters[Math.floor(Math.random() * characters.length)];
-}
-
-const randomFunc = {
-	lower: getRandomLower,
-	upper: getRandomUpper,
-	number: getRandomNumber,
-	character: getRandomCharacter
-}
-
-// function for generating password 
-function generatePassword(lower, upper, number, character) {
-
-	let generatedPassword = '';
-	const typesCount = lower + upper + number + character;
-	const typesArr = [{lower}, {upper}, {number}, {character}].filter(item => Object.values(item)[0]);
+// password length and characters are determined by the value of the password length input and by what boxes are checked
+generateBtn.addEventListener('click', function () {
+	var length = +passLength.value;
+	var wantsLower = lowerCase.checked;
+	var wantsUpper = upperCase.checked;
+	var wantsNumber = numbers.checked;
+	var wantsSymbol = symbols.checked;
 	
-	if(typesCount === 0) {
-		return '';
+	alert(randomPassword(wantsLower, wantsUpper, wantsNumber, wantsSymbol, length));
+});
+
+function randomPassword(lower, upper, number, symbol, length) {
+	var generatedPassword = "";
+	var inputTypes = lower + upper + number + symbol;
+	var inputTypesArray = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0]);
+	
+	if(inputTypes === 0) {
+		return " ";
 	}
 	
 	// for loop
-	for(let i=0; i<length; i+=typesCount) {
-		typesArr.forEach(type => {
+	for(let i = 0; i < length; i ++) {
+		inputTypes++;
+
+		inputTypesArray.forEach(type => {
 			const funcName = Object.keys(type)[0];
-			generatedPassword += randomFunc[funcName]();
+			generatedPassword += randomCharacters[funcName]();
 		});
 	}
 	
 	const finalPassword = generatedPassword.slice(0, length);
 	
-    return finalPassword;
+	return finalPassword;
 }
 
-// when you click the generate button the password prompts to the user
-generate.addEventListener('click', () => {
+// functions to pull random characters from strings
+function randomLowerCase() {
+	var lowerCase = "abcdefghijklmnopqrstuvxyz"
+	return lowerCase[Math.floor(Math.random() * lowerCase.length)];
+}
 
-	const passLength = +length.value;
-	const hasLower = lowerCase.checked;
-	const hasUpper = upperCase.checked;
-	const hasNumber = numbers.checked;
-    const hasCharacter = specialCharacters.checked;
-    
-    console.log = (generatePassword(passLength, hasLower, hasUpper, hasNumber, hasCharacter));
-    
-    // alert(generatePassword(hasLower, hasUpper, hasNumber, hasCharacter, passLength));
-});
+function randomUpperCase() {
+	var upperCase = "ABCDEFGHIJKLMKOPQRSTUVWXYZ"
+	return upperCase[Math.floor(Math.random() * upperCase.length)];
+}
+
+function randomNumber() {
+	var numbers = "0123456789"
+	return numbers[Math.floor(Math.random() * numbers.length)];
+}
+
+function randomSymbol() {
+	const symbols = '!@#$%^&*(){}[]=<>/,.+='
+	return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+// placing all random character generating functions in a single variable 
+var randomCharacters = {
+	lower: randomLowerCase,
+	upper: randomUpperCase,
+	number: randomNumber,
+	symbol: randomSymbol
+}
